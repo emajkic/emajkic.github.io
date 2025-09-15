c7afe4f9cf942bcb9b129c2b2f3d0180d74c9931
Ella Majkic <emajkic@student.ubc.ca>
2025-09-14 23:58:05 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Git", "Python", "Java", "Python", "C++", "Linux", "KiCAD", "ROOT", "ROS"],
+  skills: ["Git", "Python", "Java", "C++", "Linux", "KiCAD", "ROOT", "ROS", "MATLAB", "OnShape"],
   projects: [
     {
       name: "AI Dev Roundup Newsletter",
