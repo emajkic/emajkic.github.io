b4723473ca87d9019a25971c30798d195c66be51
Ella Majkic <emajkic@student.ubc.ca>
2025-09-19 13:55:37 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -59,7 +59,8 @@ export const siteConfig = {
       company: "TRIUMF Particle Accelerator",
       title: "Student Research Assistant - PIONEER Experiment",
       dateRange: "Jan 2025 - May 2025",
-      pdf: "../public/final_report.pdf",
+      pdf1: "../public/final_report.pdf",
+      pdf2: "../public/final_poster.pdf",
       bullets: [
         "PUT FINAL REPORT LINK INNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
         "Reduced API response times by 40% through optimization",
